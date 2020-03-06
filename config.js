4f0fd92d53977fa07ab43ca9b7c53b52a4a2dc3f
MintyU <deathcat@outlook.kr>
2020-03-06 21:52:27 +0900

    #add Search Console tag

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
 
   /** Optional */
   googleAnalytics: 'UA-147550571-2',     // Google Analytics TrackingID. ex.'UA-123456789-0'
-  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
+  googleSearchConsole: 'bYFanhcTFeHXnIcw7Yg7enLJAhg85wq_zt5hXen5Tlg', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
   googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
   googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
